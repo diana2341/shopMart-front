2606f189b6109715be04f1aaa58db9f086fbf8ff
diana2341 <dianaponce2341@gmail.com>
2020-09-20 19:16:00 -0400

    provider

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Switch } from "react-router";
 import Home from './components/Home'
 import Navbar from './components/Navabar'
 import Login from './components/Login'
+import{createStore,combineReducers} from 'redux'
+import{Provider,connect} from 'react-redux'
 
 function App() {
   return (
