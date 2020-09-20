b7231efcfadd382760dfe5111aba527a4efb69b1
Samanthaponce5 <samanthaponce5@gmail.com>
2020-09-20 09:43:31 -0400

    log

--- src/components/Navabar.js
+++ src/components/Navabar.js
@@ -7,7 +7,7 @@ render(){
         <nav>
         <ul>
         <li><Link to='/'>Home</Link></li> 
-        <li><Link to=''/></li> 
+        <li><Link to='/login'>Login</Link></li> 
         <li><Link to=''/></li> 
         <li><Link to=''/></li> 
         </ul>
