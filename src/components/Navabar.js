63cf4fce95faacca160384adc29a1019f771d374
Samanthaponce5 <samanthaponce5@gmail.com>
2020-09-20 09:18:20 -0400

    home

---
+++ src/components/Navabar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+class Navabar extends React.Component{
+
+render(){
+    return(
+        <nav>
+        <ul>
+        <li><Link to='/'>Home</Link></li> 
+        <li><Link to=''/></li> 
+        <li><Link to=''/></li> 
+        <li><Link to=''/></li> 
+        </ul>
+        </nav>
+    )
+}
+}
+
+
+
+export default Navabar
