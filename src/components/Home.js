eeb62b6afe8c90a1895f1e07595c9c8bdbbdcd54
Samanthaponce5 <samanthaponce5@gmail.com>
2020-10-04 14:00:32 -0400

    nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,102 +1,14 @@
 import React from 'react'
-import {fetchProducts} from '../actions/index'
-import {pruductShow} from '../actions/index'
-import Filter from '../components/Filter'
-import {connect} from 'react-redux'
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import { CardMedia } from '@material-ui/core';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-
-
 
 class Home extends React.Component{
- 
-componentDidMount(){
-   this.props.fetchProducts()
-}
-renderProducts=()=>{
-    const styles = {
-        card: {
-          maxWidth: 355,
-        },
-        media: {
-          height: '15rem',
-          paddingTop: '56.25%', // 16:9
-        },
-      };
-      var cardStyle = {
-        display: 'block',
-        width: '30rem',
-        transitionDuration: '0.3s',
-        height: '30rem'
-    }
-    
-    
 
-    return this.props.products.map(product=>{
-
+  render(){
     return(
-    <React.Fragment key={product.id}>
-
-  <Card style={cardStyle} >
-        <CardHeader
-        title={product.name}
-      />
- <CardMedia   
-   style={styles.media} 
-   image={product.images}   
- /> 
-  <CardContent>
-        <Typography  variant="h4" color="primary" component="p">
-          ${product.price}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button variant="outlined" color="primary"size="small" >
-          add to cart
-        </Button>
-        <Button 
-        onClick={()=>pruductShow(product.id)}
-        size="small" 
-        variant="outlined"
-        color="primary">
-          see details
-        </Button>
-      </CardActions>
-        </Card>
-
-    </React.Fragment>)
-    })
-}
-    render(){
-        return(
-          
-        <div >
-          <div className='filter-row'>
-            <Filter/>
-          </div>
-          
-          <div className='row'>
-           {this.renderProducts()} 
-          </div>
-          
-
-        </div>
-        )
-    }
-}
-
-const mapStateToProps=(state)=>{
-   return{
-      products:state.products.filteredProducts,
-
-
+      <div>
+        Home
+      </div>
+    )
   }
-
 }
 
-export default connect(mapStateToProps,{fetchProducts,pruductShow})(Home) 
+export default Home
--- src/components/Navabar.js
+++ src/components/Navabar.js
@@ -1,122 +1,141 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
-import {signOut} from '../actions'
-import NavFilter from './NavFilter'
-import '../App.css'
-import {BiShoppingBag} from 'react-icons/bi'
-import logo from '../img/LogoMakr_4bjfzb.png'
-import {IoIosArrowForward} from 'react-icons/io'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../actions';
+import NavFilter from './NavFilter';
+import '../App.css';
+import { BiShoppingBag } from 'react-icons/bi';
+import logo from '../img/LogoMakr_4bjfzb.png';
+import { IoIosArrowForward } from 'react-icons/io';
 
-class Navabar extends React.Component{
+class Navabar extends React.Component {
+	state = {
+		active: false,
+	};
 
-    state={
-        active:false
-    }
+	handleLogOut = () => {
+		this.props.signOut();
+	};
+	toggle = () => {
+		if (!this.state.active) {
+			this.setState({ active: !this.state.active });
+			document.querySelector('.navLinks').style.right = '0';
+		} else {
+			this.setState({ active: !this.state.active });
 
- 
+			document.querySelector('.navLinks').style.right = '-100%';
+		}
+	};
+	handleSelect = (e) => {
+		if (
+			e.target.className === 'home' ||
+			e.target.className.baseVal === 'home arrow one'
+		) {
+			document.querySelector('.navLinks').style.right = '-100%';
+			this.setState({ active: !this.state.active });
+		}
+	};
 
-    handleLogOut=()=>{
-        this.props.signOut()
-    }
-    toggle=()=>{
-        if(!this.state.active ){
-            this.setState({active:!this.state.active})
-            document.querySelector('.navLinks').style.right='0'
-        }else{
-            this.setState({active:!this.state.active})
+	render() {
+		console.log(this.props.currentUser.user);
+		return (
+			<>
+				<nav>
+					<div className="logo">
+						<Link to="/">
+							<img className="logoImg" src={logo} />
+						</Link>
+					</div>
+					<ul className="navLinks">
+						<li>
+							<input className="search" type="text" placeholder="Search..." />
+						</li>
 
-            document.querySelector('.navLinks').style.right='-100%'
-        }
-    
-    }
-    handleSelect=(e)=>{
-    
-        if(e.target.className==='home'|| e.target.className.baseVal==='home arrow one' ){
-            document.querySelector('.navLinks').style.right='-100%'
-            this.setState({active:!this.state.active})
-        }
-    }
-  
-render(){
-    console.log(this.props.currentUser.user)
-    return(
-        <>
-        <nav>
-        <div className='logo'><Link to='/'><img  className='logoImg' src={logo}/></Link></div>
-        <ul className='navLinks' >
-        <li><input className='search' type='text' placeholder='Search...'/></li> 
-        
-        <li onClick={this.handleSelect} className='home'><Link className='home' to='/'>Home <IoIosArrowForward size={30} className='home arrow one'/></Link></li> 
-        <div className='right-menu two'>
+						<li onClick={this.handleSelect} className="home">
+							<Link className="home" to="/">
+								Home <IoIosArrowForward size={30} className="home arrow one" />
+							</Link>
+						</li>
+						<div className="right-menu two">
+							<li className="menu-button women">
+								<Link to="" />
+								Women <IoIosArrowForward size={30} className="arrow two" />
+							</li>
+							<li className="menu-button men">
+								<Link to="" />
+								Men <IoIosArrowForward size={30} className="arrow three" />
+							</li>
+							<li className="menu-button kids">
+								<Link to="" />
+								Kids <IoIosArrowForward size={30} className="arrow four" />
+							</li>
 
-        <li  className='menu-button women'><Link to=''/>Women  <IoIosArrowForward size={30} className='arrow two'/></li> 
-        <li  className='menu-button men'><Link to=''/>Men  <IoIosArrowForward size={30} className='arrow three'/></li> 
-        <li  className='menu-button kids'><Link to=''/>Kids  <IoIosArrowForward size={30} className='arrow four'/></li> 
-        
-        <div className='dropdown-menu two'>
-            {/* you can change anything form here */}
-        <ul>
-            <label>
-            <b>Clothing</b>
-        <li>Tops</li>
-        <li>Jackets & Outerwear</li>
-        <li>Jeans</li>
-        <li>SportsWear</li>
-        <li>Lingerie & Sleep</li>
+							<div className="dropdown-menu two">
+								{/* you can change anything form here */}
+								<ul>
+									<label>
+										<b>Clothing</b>
+										<li>Tops</li>
+										<li>Jackets & Outerwear</li>
+										<li>Jeans</li>
+										<li>SportsWear</li>
+										<li>Lingerie & Sleep</li>
+									</label>
 
-        </label>
-       
-        
-        <label><b>Shoes & Accessories</b>
-        <li>Jewerly</li>
-        <li>Hair Accessories</li>
-        <li>Beauty</li>
-        <li>Shoes</li>
-        <li>Socks</li>
-        </label>
-        {/* to hereeeee */}
-        </ul>
-        </div>
-        </div>
-        {this.props.currentUser.user?
-        <>
-        <div className='right-menu'>
-        <li className='menu-button'>Hello {this.props.currentUser.user.first_name} <IoIosArrowForward size={30} className='arrow five'/></li>
-        <div className='dropdown-menu'>
+									<label>
+										<b>Shoes & Accessories</b>
+										<li>Jewerly</li>
+										<li>Hair Accessories</li>
+										<li>Beauty</li>
+										<li>Shoes</li>
+										<li>Socks</li>
+									</label>
+									{/* to hereeeee */}
+								</ul>
+							</div>
+						</div>
+						{this.props.currentUser.user ? (
+							<>
+								<div className="right-menu">
+									<li className="menu-button">
+										Hello {this.props.currentUser.user.first_name}{' '}
+										<IoIosArrowForward size={30} className="arrow five" />
+									</li>
+									<div className="dropdown-menu">
+										<li>Edit Profile</li>
 
-        <li >Edit Profile</li>
+										<li>WishList</li>
 
-        <li>WishList</li>
+										<li onClick={this.handleLogOut}>Log Out</li>
+									</div>
+								</div>
+							</>
+						) : (
+							<li>
+								<Link to="/login">Login</Link>
+							</li>
+						)}
+						<li>
+							<Link to="/cart">
+								<BiShoppingBag size={22} />{' '}
+								<IoIosArrowForward size={30} className="arrow six" />
+							</Link>
+						</li>
+					</ul>
 
-        <li  onClick={this.handleLogOut}>Log Out</li>
-        </div>
-        </div>
-        </>
-        : 
-        <li><Link to='/login'>Login</Link></li>} 
-        <li><Link to='/cart'><BiShoppingBag size={22}/> <IoIosArrowForward size={30} className='arrow six'/></Link></li> 
-        </ul>
-
-            <div onClick={this.toggle}  className='burger'>
-                <div className='line1'></div>
-                <div className='line2'></div>
-                <div className='line3'></div>
-            </div>
-        </nav>
-    </>
-    )
-  }
+					<div onClick={this.toggle} className="burger">
+						<div className="line1"></div>
+						<div className="line2"></div>
+						<div className="line3"></div>
+					</div>
+				</nav>
+			</>
+		);
+	}
 }
 
+const mapStateToProps = (state) => {
+	return { currentUser: state.auth };
+};
 
-
-
-
-
-const mapStateToProps=(state)=>{
-    return ({currentUser:state.auth})
-}
-
-
-export default connect(mapStateToProps,{signOut})(Navabar)
+export default connect(mapStateToProps, { signOut })(Navabar);
