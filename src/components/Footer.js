46c3d6297cbd429b8001c031ac4c65d4d520cb18
diana2341 <dianaponce2341@gmail.com>
2020-10-09 20:25:47 -0400

    cards

---
+++ src/components/Footer.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import PaymentIcon from 'react-payment-icons';
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div class="footer">
+                <div className='con'>
+                    <ul>
+                    <p>The Company</p>
+                    <li>About us</li> 
+                    <li>Return Poilicy</li>
+                    <li>FAQ</li>
+                </ul>
+
+                <ul>
+                    <p>Shop Departments</p>
+                    <li>Women</li>
+                    <li>Girls</li>
+                    <li>Boys</li>
+                    <li>Men</li>
+                </ul>  
+                </div>
+              <div className='pay-card'>
+            <p className='card'>WE ACCEPT:</p>
+              <div className='payments'>
+                    <div className='pay'>
+                    <span>
+                        <PaymentIcon
+                        id="visa"
+                        style={{ margin: 10, width: 70 }}
+                        className="payment-icon"
+                        />
+                    </span>
+                    <span>
+                        <PaymentIcon
+                        id="discover"
+                        style={{ margin: 10, width: 70 }}
+                        className="payment-icon"
+                        />
+                    </span>
+                    <span>
+                        <PaymentIcon
+                        id="maestro"
+                        style={{ margin: 10, width: 70 }}
+                        className="payment-icon"
+                        />
+                    </span>
+                    <span>
+                        <PaymentIcon
+                        id="paypal"
+                        style={{ margin: 10, width: 70 }}
+                        className="payment-icon"
+                        />
+                    </span>
+                    </div>
+                    <span>
+                        <PaymentIcon
+                        id="western"
+                        style={{ margin: 10, width: 70 }}
+                        className="payment-icon"
+                        />
+                    </span>
+                   
+                    <span>
+                        <PaymentIcon
+                        id="wallet"
+                        style={{ margin: 10, width: 70 }}
+                        className="payment-icon"
+                        />
+                    </span>
+                    <span>
+                        <PaymentIcon
+                        id="mastercard"
+                        style={{ margin: 10, width: 70 }}
+                        className="payment-icon"
+                        />
+                    </span>
+                </div> 
+              </div>
+                <p className='rights'>© 2020 ShopMart USA, Inc. All rights reserved.</p>
+            </div>
+        )
+    }
+}
+export default Footer
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,33 @@ import React from 'react'
 class Home extends React.Component{
 
   render(){
+    let choices=['shoes','sweaters','jeans','sports']
     return(
       <div>
-        Home
+        <br/>
+       <img className='banner' src={require('../img/logo2.png')}/>
+       <p className='wow'><i className='w'>WOW!</i> Finds in Store</p>
+
+       <div className='category'>
+         {choices.map(choice=>{
+          return (
+            <>
+            <img className={`choose ${choice}`} src={require(`../img/${choice}.png`)}/>
+            </>
+
+          )
+
+         })}
+         <p className='b-b'>Shop <span className='w'>Beauty</span> products </p>
+   
+         <img className='beauty banner' src={require(`../img/beauty.png`)}/>
+
+
+
+      </div>
+  
+      
+      
       </div>
     )
   }
