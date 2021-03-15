01228d32d74c329269d317159b83c024a53fb6ab
Samanthaponce5 <samanthaponce5@gmail.com>
2021-03-14 22:34:44 -0400

    axios

--- src/apis/Store.js
+++ src/apis/Store.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL:'http://localhost:3001/'
-    // baseURL:' https://shopmartback.herokuapp.com'
+    baseURL:' https://shopmartback.herokuapp.com'
 })
